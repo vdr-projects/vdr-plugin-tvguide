9f127fbbb5e0cc508879bb380f214d72db9439e8
louis <louis.braun@gmx.de>
2014-02-01 07:54:01 +0100

    Version 1.2.0

--- tvguide.c
+++ tvguide.c
@@ -26,7 +26,7 @@
 #error "VDR-2.0.0 API version or greater is required!"
 #endif
 
-static const char *VERSION        = "1.2.0pre";
+static const char *VERSION        = "1.2.0";
 static const char *DESCRIPTION    = "A fancy 2d EPG Viewer";
 static const char *MAINMENUENTRY  = "Tvguide";
 
